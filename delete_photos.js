25b04ca3d706746e8abacabc18722b73f463e1e7
recursionbane <9662059+recursionbane@users.noreply.github.com>
2023-01-13 12:47:30 -0800

    Intelligent wait until current deletion completes; no limit on trries

--- delete_photos.js
+++ delete_photos.js
@@ -1,3 +1,5 @@
+// This Javascript code will delete all photos from Google Photos
+
 // How many photos to delete?
 // Put a number value, like this
 // const maxImageCount = 5896
@@ -13,11 +15,9 @@ const ELEMENT_SELECTORS = {
 
 // Time Configuration (in milliseconds)
 const TIME_CONFIG = {
-    delete_cycle: 10000,
-    press_button_delay: 2000
+    delete_cycle: 10000, // How often to check if the previous deletion has completed
+    press_button_delay: 2000 // How long to wait before clicking the delete button
 };
-
-const MAX_RETRIES = 10;
 
 let imageCount = 0;
 
@@ -27,26 +27,17 @@ let buttons = {
     confirmationButton: null
 }
 
-let deleteTask = setInterval(() => {
-    let attemptCount = 1;
-
+let deleteTaskSingle = function () {
     do {
         checkboxes = document.querySelectorAll(ELEMENT_SELECTORS['checkboxClass']);
 
     } while (checkboxes.length <= 0 && attemptCount++ < MAX_RETRIES);
 
-
-    if (checkboxes.length <= 0) {
-        console.log("[INFO] No more images to delete.");
-        clearInterval(deleteTask);
-        console.log("[SUCCESS] Tool exited.");
-        return;
-    }
-
     imageCount += checkboxes.length;
 
     checkboxes.forEach((checkbox) => { checkbox.click() });
     console.log("[INFO] Deleting", checkboxes.length, "images");
+
 
     setTimeout(() => {
         try {
@@ -71,4 +62,17 @@ let deleteTask = setInterval(() => {
 
         }, TIME_CONFIG['press_button_delay']);
     }, TIME_CONFIG['press_button_delay']);
+}
+
+let deleteTask = setInterval(() => {  
+    // Parse the entire DOM to find the string "Moving <number> items to trash"
+    // If found, wait until this DOM element is removed
+    // This is to prevent the script from clicking the delete button before the previous delete operation is completed
+    let movingToTrashElement = document.evaluate("//*[contains(text(), 'Moving')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (movingToTrashElement) {
+        console.log("[INFO] Waiting for previous delete operation to complete");
+    } else {
+        console.log("[INFO] Previous delete operation completed");
+        deleteTaskSingle();
+    }
 }, TIME_CONFIG['delete_cycle']);
